8b49d0b6cf3516731d5babb01d746f19c5f573cc
Nazlıcan Çay <nazlican1306@gmail.com>
2023-09-16 20:35:09 +0300

    displaying portfolio infos

--- Trader/Trader/Controllers/PortfolioViewController.swift
+++ Trader/Trader/Controllers/PortfolioViewController.swift
@@ -7,41 +7,79 @@
 
 import UIKit
 
-class PortfolioViewController: UIViewController {
-
+class PortfolioViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var PortfoliosTableView: UITableView!
     var defaultAccount: String?
     var userName: String?
     var userPassword: String?
-    
+    var portfolioItems: [PortfolioItem] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let defaultAccount = defaultAccount {
-            print("Gelen Default Account: \(defaultAccount)")
-            print("Gelen username: \(userName)")
-            print("Gelen password: \(userPassword)")
-        }
+        PortfoliosTableView.dataSource = self
+        PortfoliosTableView.delegate = self
+        
         
         if let defaultAccount = defaultAccount,
            let username = userName,
            let password = userPassword {
             NetworkManager.shared.getPortfolio(for: defaultAccount, username: username, password: password) { result in
-                switch result {
-                case .success(let portfolioResponse):
-                    if portfolioResponse.Result.State {
-                        for item in portfolioResponse.Item {
-                            print("AccountID: \(item.AccountID), Symbol: \(item.Symbol), Qty_T: \(item.Qty_T2), Qty_T1: \(item.LastPx) , Tutar:\(Int(item.LastPx*item.Qty_T2))")
+                DispatchQueue.main.async {
+                    switch result {
+                    case .success(let portfolioResponse):
+                        if portfolioResponse.Result.State {
+                            self.portfolioItems = portfolioResponse.Item
+                            self.PortfoliosTableView.reloadData()
+                            for item in portfolioResponse.Item {
+                                print("lastPx: \(item.LastPx), Symbol: \(item.Symbol), Qty_T2: \(item.Qty_T2), Tutar: \(Int(item.Qty_T2 * item.LastPx))")
+
+                            }
+                        } else {
+                            print("Hata: \(portfolioResponse.Result.Description)")
                         }
-                    } else {
-                        print("Hata: \(portfolioResponse.Result.Description)")
+                    case .failure(let error):
+                        print("Hata: \(error)")
                     }
-                case .failure(let error):
-                    print("Hata: \(error)")
                 }
             }
         }
+        
+       
+        
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return portfolioItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print("cellForRowAt is being called")
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PortfolioCellIdentifier", for: indexPath) as! PortfolioTableViewCell
+        let portfolioItem = portfolioItems[indexPath.row]
+        cell.CostLabel.textColor = .black
+        cell.LastPxLabel.textColor = .black
+        cell.Qty_T2Label.textColor = .black
+        
+        cell.SymbolLabel.text = String(portfolioItem.Symbol)
+        cell.Qty_T2Label.text = String(portfolioItem.Qty_T2)
+        cell.LastPxLabel.text = String(portfolioItem.LastPx)
+        cell.CostLabel.text   = String (Int(portfolioItem.Qty_T2 * portfolioItem.LastPx))
+        
+        
+        return cell
+           
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+
+    
+   
+
         
     
     /*
---
+++ Trader/Trader/Models/PortfolioTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  PortfolioTableViewCell.swift
+//  Trader
+//
+//  Created by Nazlıcan Çay on 16.09.2023.
+//
+
+import UIKit
+
+class PortfolioTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var SymbolLabel: UILabel!
+    
+    @IBOutlet weak var Qty_T2Label: UILabel!
+    
+    @IBOutlet weak var LastPxLabel: UILabel!
+    
+    @IBOutlet weak var CostLabel: UILabel!
+    
+    
+}
