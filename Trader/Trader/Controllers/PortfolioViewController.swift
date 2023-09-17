df23fdc0e4a785928823ce8bb64362e092f4908d
Nazlıcan Çay <nazlican1306@gmail.com>
2023-09-17 15:37:37 +0300

    sortbyButton

--- Trader/Trader/Controllers/PortfolioViewController.swift
+++ Trader/Trader/Controllers/PortfolioViewController.swift
@@ -7,7 +7,8 @@
 
 import UIKit
 
-class PortfolioViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class PortfolioViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
+  
     
     @IBOutlet weak var PortfoliosTableView: UITableView!
     var defaultAccount: String?
@@ -15,11 +16,20 @@ class PortfolioViewController: UIViewController, UITableViewDataSource, UITableV
     var userPassword: String?
     var portfolioItems: [PortfolioItem] = []
     
+    let pickerData = ["Alfabetik Sırala", "Artan Fiyat", "Azalan Fiyat"]
+    let pickerView = UIPickerView()
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         PortfoliosTableView.dataSource = self
         PortfoliosTableView.delegate = self
+        
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        
+        pickerView.selectRow(0, inComponent: 0, animated: true)
         
         PortfoliosTableView.register(PortfolioTableHeaderView.self, forHeaderFooterViewReuseIdentifier: "PortfolioHeader")
 
@@ -67,7 +77,11 @@ class PortfolioViewController: UIViewController, UITableViewDataSource, UITableV
         cell.SymbolLabel.text = String(portfolioItem.Symbol)
         cell.Qty_T2Label.text = String(portfolioItem.Qty_T2)
         cell.LastPxLabel.text = String(portfolioItem.LastPx)
-        cell.CostLabel.text   = String (Int(portfolioItem.Qty_T2 * portfolioItem.LastPx))
+        
+        if let formattedTotalCost = formatNumber(portfolioItem.Qty_T2 * portfolioItem.LastPx) {
+            cell.CostLabel.text = formattedTotalCost
+        }
+
         
         
         return cell
@@ -99,10 +113,13 @@ class PortfolioViewController: UIViewController, UITableViewDataSource, UITableV
     
     func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
         let footerView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.size.width, height: 40))
-        footerView.backgroundColor = .gray // veya istediğiniz bir renk
+        footerView.backgroundColor = .gray 
 
         let label = UILabel(frame: CGRect(x: 10, y: 5, width: tableView.frame.size.width - 20, height: 30))
-        label.text = "Toplam Tutar: \(Int(hesaplaToplamTutar()))"
+        label.textColor = .white
+        if let formattedTotal = formatNumber(hesaplaToplamTutar()) {
+            label.text = "Toplam Tutar: \(formattedTotal)"
+        }
         footerView.addSubview(label)
 
         return footerView
@@ -111,6 +128,65 @@ class PortfolioViewController: UIViewController, UITableViewDataSource, UITableV
     func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
         return 40
     }
+    
+    // MARK: - UIPickerViewDelegate & UIPickerViewDataSource Methods
+
+        func numberOfComponents(in pickerView: UIPickerView) -> Int {
+            return 1
+        }
+
+        func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+            return pickerData.count
+        }
+
+        func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+            return pickerData[row]
+        }
+
+        func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+            switch row {
+            case 0:
+                self.portfolioItems.sort { $0.Symbol < $1.Symbol }
+            case 1:
+                self.portfolioItems.sort { $0.Qty_T2 * $0.LastPx < $1.Qty_T2 * $1.LastPx }
+            case 2:
+                self.portfolioItems.sort { $0.Qty_T2 * $0.LastPx > $1.Qty_T2 * $1.LastPx}
+            default:
+                break
+            }
+            self.PortfoliosTableView.reloadData()
+            self.dismiss(animated: true, completion: nil)
+        }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 50.0
+    }
+
+
+    //MARK: - tableview SortByBtn function
+    @IBAction func SortByBtn(_ sender: UIButton) {
+            self.view.endEditing(true)
+
+            let alert = UIAlertController(title: "\n\n\n\n\n\n", message: nil, preferredStyle: .actionSheet)
+            alert.view.addSubview(pickerView)
+
+            let cancel = UIAlertAction(title: "İptal", style: .cancel, handler: nil)
+            alert.addAction(cancel)
+
+            present(alert, animated: true, completion: { [unowned self] in
+                self.pickerView.frame = CGRect(x: 17, y: 10, width: alert.view.frame.width - 50 , height: 120)
+            })
+        }
+    
+    //MARK: - function to diplay numeric datas more easy to read
+    func formatNumber(_ number: Double) -> String? {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.maximumFractionDigits = 0
+        formatter.groupingSeparator = "."
+        return formatter.string(from: NSNumber(value: number))
+    }
+
 
 
         
